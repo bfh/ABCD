b37f828b96d147236e5f6ad52d0fca7ed4a095b7
ABCD <test@test.com>
2016-10-25 13:51:17 +0000

    change for pi

--- BackupScript.sh
+++ BackupScript.sh
@@ -2,6 +2,6 @@
 
 DATE='$date +"%y-%m-%d"'
 
-tar -cvpPzf Backup-$(date +%Y-%m-%d).tar.gz /run/user/1000/gvfs/*/*/Ringtones 
+tar -cvpPzf Backup-$(date +%Y-%m-%d).tar.gz /run/user/1000/gvfs/*/*/ 
 
-echo
+echo DONE
